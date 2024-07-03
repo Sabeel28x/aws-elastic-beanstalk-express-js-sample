e3f86fbde5c9cdb879350d86bcb838edcebd4e4d
root <root@ip-172-31-39-251.ap-south-1.compute.internal>
2024-07-03 04:03:21 +0000

    new app

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
 const app = express();
-const port = 8080;
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.get('/', (req, res) => res.send('Full Pipeline Test'));
+const PORT = process.env.PORT || 3000;
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+let users = {};
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle new user joining
+    socket.on('new user', (username) => {
+        users[socket.id] = username;
+        io.emit('user list', Object.values(users));
+        socket.broadcast.emit('user joined', username);
+    });
+
+    // Broadcast message to all users
+    socket.on('chat message', (msg) => {
+        const username = users[socket.id];
+        io.emit('chat message', { user: username, text: msg });
+    });
+
+    socket.on('disconnect', () => {
+        const username = users[socket.id];
+        delete users[socket.id];
+        io.emit('user list', Object.values(users));
+        socket.broadcast.emit('user left', username);
+        console.log('User disconnected');
+    });
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
